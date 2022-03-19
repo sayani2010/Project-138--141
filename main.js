520db95db4a6134e68e4c7010ef8846c1bcdbfc3
sayani2010 <85400495+sayani2010@users.noreply.github.com>
2022-03-19 15:54:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+
+
+rightWristY = 0;
+rightWristX = 0;
+scoreRightWrist = 0;
+
+game_status = "";
+
+ function preload() {
+
+}
+
+function setup()
+  {
+    var canvas =  createCanvas(700,600);
+    canvas.parent('canvas');
+    
+    video = createCapture(VIDEO);
+    video.size(700, 600);
+    video.hide();
+    
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+  }
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+
+    rightWristY = results[0].pose.rightWrist.y;
+    rightWristX = results[0].pose.rightWrist.x;
+    scoreRightWrist =  results[0].pose.keypoints[10].score;
+    console.log(scoreRightWrist);
+  }
+}
+
+function startGame()
+{
+  game_status = "start";
+  document.getElementById("status").innerHTML = "Game Is Loading";
+}
+function draw(){
+
+  background(0); 
+  image(video, 0, 0, 700, 600);
+}
+
